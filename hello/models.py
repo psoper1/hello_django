e712c420246f96e478165955f79fb6379f330fc8
psoper1 <psoper0604@gmail.com>
2023-05-31 18:17:13 -0400

    test

--- hello/models.py
+++ hello/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class champ(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField(null=True)
-    # image = models.ImageField(upload_to='champ_images/', null=True)
     image = models.CharField(max_length=2000, null=True)
 
     class Meta:
