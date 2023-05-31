f6dc2b8f5a836ead6c1d4a161792c92168dd8953
psoper1 <psoper0604@gmail.com>
2023-05-31 18:06:53 -0400

    trying to fix images

--- hello/urls.py
+++ hello/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from hello import views
-from .views import ChampViewSet
+from .views import ChampListCreateView, ChampDetailAPIView
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path('champs/', ChampViewSet.as_view({'get': 'list'}), name='champ-list'),
-    path('champs/<int:pk>/', ChampViewSet.as_view({'get': 'retrieve'}), name='champ-detail'),
+    # path('champs/', ChampViewSet.as_view({'get': 'list'}), name='champ-list'),
+    path('champs/', ChampListCreateView.as_view(), name='champ-list-create'),
+    path('champs/<int:pk>/', ChampDetailAPIView.as_view(), name='champ-detail'),
     # Other URL patterns for your app...
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import champ
 from .serializers import ChampSerializer
 
 def home(request):
     return HttpResponse("Hello, Django!")
 
-class ChampViewSet(viewsets.ModelViewSet):
+class ChampDetailAPIView(generics.RetrieveAPIView):
+    queryset = champ.objects.all()
+    serializer_class = ChampSerializer
+
+class ChampListCreateView(generics.ListCreateAPIView):
     queryset = champ.objects.all()
     serializer_class = ChampSerializer
