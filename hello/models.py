f91559adf65288947fc5db1f31b0f943ea232851
psoper1 <psoper0604@gmail.com>
2023-06-03 12:56:54 -0400

    added fields I may use later

--- hello/models.py
+++ hello/models.py
@@ -4,9 +4,11 @@ from django.db import models
 
 class champ(models.Model):
     name = models.CharField(max_length=100)
-    # age = models.IntegerField(null=True)
     image = models.CharField(max_length=2000, null=True)
     splash = models.CharField(max_length=2000, null=True)
+    # description = models.CharField(max_length=5000, null=True)
+    # qTitle = models.CharField(max_length=100, null=True)
+    # qDescription = models.CharField(max_length=1000, null=True)
 
     def __str__(self):
         return self.name
