abb56ba207a8132781b9368281cd6a7a1e227136
psoper1 <psoper0604@gmail.com>
2023-06-05 09:37:21 -0400

    fixed champ model

--- hello/models.py
+++ hello/models.py
@@ -6,6 +6,7 @@ class champ(models.Model):
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=2000, null=True)
     splash = models.CharField(max_length=2000, null=True)
+    lane = models.ForeignKey('Lane', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.name
