01b6812227b2f2106a583d572049f02d137720c3
psoper1 <psoper0604@gmail.com>
2023-06-05 11:53:05 -0400

    updated lane model

--- hello/models.py
+++ hello/models.py
@@ -6,7 +6,7 @@ class champ(models.Model):
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=2000, null=True)
     splash = models.CharField(max_length=2000, null=True)
-    lane = models.ForeignKey('Lane', on_delete=models.PROTECT, null=True)
+    lane = models.ManyToManyField('lane')
 
     def __str__(self):
         return self.name
@@ -15,7 +15,7 @@ class champ(models.Model):
         ordering = ['name']
 
 class lane(models.Model):
-    lane = models.CharField(max_length=100)
+    lane = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.lane
--- hello/serializers.py
+++ hello/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
-class ChampSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = champ
-        fields = '__all__'
 
 class LaneSerializer(serializers.ModelSerializer):
     class Meta:
         model = lane
+        fields = '__all__'
+
+class ChampSerializer(serializers.ModelSerializer):
+    lane = LaneSerializer(many=True)
+    class Meta:
+        model = champ
         fields = '__all__'
