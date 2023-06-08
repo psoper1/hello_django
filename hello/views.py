55880aca324719382a55e7adad47b313340ba7e3
psoper1 <psoper0604@gmail.com>
2023-06-08 10:19:46 -0400

    setup API

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 from rest_framework import viewsets, generics
 from .models import champ, lane
 from .serializers import ChampSerializer, LaneSerializer
+from django.http import JsonResponse
 
 def home(request):
     return HttpResponse("Hello, Django!")
@@ -22,3 +24,7 @@ class LaneListCreateView(generics.ListCreateAPIView):
 class LaneDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = lane.objects.all()
     serializer_class = LaneSerializer
+
+def get_riot_api_key(request):
+    api_key = settings.RIOT_API_KEY
+    return JsonResponse({'api_key': api_key})
