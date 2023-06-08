b70d429bb65cf41f432ada02acaba9ffc65951e1
psoper1 <psoper0604@gmail.com>
2023-06-08 11:09:51 -0400

    fixed apikey calls

--- hello/views.py
+++ hello/views.py
@@ -5,6 +5,8 @@ from rest_framework import viewsets, generics
 from .models import champ, lane
 from .serializers import ChampSerializer, LaneSerializer
 from django.http import JsonResponse
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 
 def home(request):
     return HttpResponse("Hello, Django!")
@@ -25,6 +27,10 @@ class LaneDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = lane.objects.all()
     serializer_class = LaneSerializer
 
+@cache_page(60 * 60)  # Cache the response for 1 hour
 def get_riot_api_key(request):
-    api_key = settings.RIOT_API_KEY
+    api_key = cache.get('riot_api_key')
+    if api_key is None:
+        api_key = settings.RIOT_API_KEY
+        cache.set('riot_api_key', api_key)
     return JsonResponse({'api_key': api_key})
