b714c936d312ea8d02c9b4a0ddc8f28827ebadbd
psoper1 <psoper0604@gmail.com>
2023-06-14 21:57:01 -0400

    format edits

--- hello/urls.py
+++ hello/urls.py
@@ -4,11 +4,9 @@ from .views import ChampListCreateView, ChampDetailAPIView, LaneListCreateView, 
 
 urlpatterns = [
     path("", views.home, name="home"),
-    # path('champs/', ChampViewSet.as_view({'get': 'list'}), name='champ-list'),
     path('champs/', ChampListCreateView.as_view(), name='champ-list-create'),
     path('champs/<int:pk>/', ChampDetailAPIView.as_view(), name='champ-detail'),
     path('lanes/', LaneListCreateView.as_view(), name='lane-list-create'),
     path('lanes/<int:pk>/', LaneDetailAPIView.as_view(), name='lane-detail'),
     path('api/get_riot_api_key/', get_riot_api_key, name='get_riot_api_key'),
-    # Other URL patterns for your app...
 ]
